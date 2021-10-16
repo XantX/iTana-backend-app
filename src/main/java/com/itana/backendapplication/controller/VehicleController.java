63cdf7e1dd6c537653773ec9fb308983fc01f06f
XantX <hp_xantx@hotmail.com>
2021-10-16 00:15:19 -0500

    Develop the rest of the crud controllers were implemented. TODO: Testing controller with postman

--- src/main/java/com/itana/backendapplication/controller/VehicleController.java
+++ src/main/java/com/itana/backendapplication/controller/VehicleController.java
@@ -1,6 +1,7 @@
 package com.itana.backendapplication.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.itana.backendapplication.entity.Vehicle;
 import com.itana.backendapplication.service.VehicleService;
@@ -9,7 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +39,41 @@ public class VehicleController {
 			}
 			
 		} catch(Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	@PutMapping(path = "/vehicle/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Vehicle> update(@RequestBody Vehicle entity){
+		try {
+			if(vehicleService.findById(entity.getId()).isPresent()){
+				Vehicle updatedVehicle = vehicleService.update(entity);
+				return new ResponseEntity<Vehicle>(updatedVehicle,HttpStatus.OK);
+			}else{
+				return new ResponseEntity<Vehicle>(HttpStatus.NOT_FOUND);
+			}
+		} catch(Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+
+	@PostMapping(path = "/vehicles/create", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Vehicle> create(@RequestBody Vehicle entity)throws Exception{
+
+		try {
+			Vehicle newVehicle = vehicleService.save(entity);
+			return new ResponseEntity<Vehicle>(newVehicle, HttpStatus.OK);
+		} catch(Exception e){
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping(path = "/vehicles/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> deleteEntity(@PathVariable long id){
+		try {
+			vehicleService.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch(Exception e){
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
