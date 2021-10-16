7576e283dc81ef1b95bb25f99f13968e2f117a83
XantX <hp_xantx@hotmail.com>
2021-10-16 14:32:26 -0500

    Develop: the resource for data input to the api has been added. TODO: Implement in controller

---
+++ src/main/java/com/itana/backendapplication/resource/SaveVehicleResource.java
@@ -0,0 +1,33 @@
+package com.itana.backendapplication.resource;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+//import lombok.Data;
+
+/**
+* SaveVehicleResource
+*/
+//@Data
+public class SaveVehicleResource {	
+
+	@NotNull
+	@NotBlank(message = "year required")
+	@Size(max=70)
+	private String year;
+
+	@NotNull
+	@NotBlank(message = "category required")
+	@Size(max=70)
+	private String category;
+
+	@NotNull
+	@NotBlank(message = "type required")
+	@Size(max=70)
+	private String type;
+
+	@NotNull
+	@NotBlank(message = "number required")
+	private Long number;
+}
