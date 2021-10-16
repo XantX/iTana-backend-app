6c2b4095d96e93bf9efc5fd40e65de23c96106a4
XantX <hp_xantx@hotmail.com>
2021-10-15 20:27:03 -0500

    Develop: the entity vehicles was added

---
+++ src/main/java/com/itana/backendapplication/entity/Vehicle.java
@@ -0,0 +1,34 @@
+package com.itana.backendapplication.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+//import lombok.Data;
+
+/**
+* Vehicle
+*/
+@Entity
+@Table(name = "vehicles")
+@Data
+public class Vehicle {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 70)
+	private String year;
+
+	@Column(length = 70)
+	private String category;
+
+	@Column(length = 70)
+	private String type;
+
+	@Column
+	private Long number;
+}
