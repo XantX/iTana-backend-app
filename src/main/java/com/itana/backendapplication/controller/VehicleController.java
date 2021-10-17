fdbe0a3262b2b4fafe5ebef1a9273d521a843d8c
XantX <hp_xantx@hotmail.com>
2021-10-16 22:22:51 -0500

    Configuration: added cors config to access to vehicles controller

--- src/main/java/com/itana/backendapplication/controller/VehicleController.java
+++ src/main/java/com/itana/backendapplication/controller/VehicleController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 */
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class VehicleController {
 	@Autowired
 	private VehicleService vehicleService;
